8c3d2e0df0585766396331873214122aafd7adf3
Mateus <mateus.lins.sales@gmail.com>
2023-08-22 00:00:10 -0300

    feat: icone da pagina

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="images/icon.ico" sizes="any" />
+      </head>
       <body className={fonte.className}>
         <ModalProvider />
         <ToastProvider />
