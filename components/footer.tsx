a072c14d2ae5b8ec59ad6e47a4876ced27a8ed6a
Mateus Sales <mateus.lins.sales@gmail.com>
2023-10-25 08:52:22 -0300

    feat: change contato para whatsapp

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,21 +1,54 @@
-import { LinkedinIcon } from "lucide-react";
+"use client";
+
+import { Mail } from "lucide-react";
 import Link from "next/link";
 
 const Footer = () => {
+  const getLink = () => {
+    let width =
+      window.innerWidth ||
+      document.documentElement.clientWidth ||
+      document.body.clientWidth;
+
+    const phoneWhats = "5581992322222";
+
+    const textWhats = `Oi, tudo bem? vi seu site da Roltec e achei muito legal!`;
+
+    const whatsMobile =
+      `whatsapp://send?phone=${phoneWhats}&text=` +
+      encodeURIComponent(textWhats);
+
+    if (width < 720) {
+      return whatsMobile;
+    }
+
+    return `https://web.whatsapp.com/send?phone=${phoneWhats}&text=${encodeURIComponent(
+      textWhats
+    )}&app_abs`;
+  };
+
   return (
     <footer className=" bg-white border-t">
       <div className="flex-col space-y-1 sm:space-y-0 sm:flex sm:flex-row justify-around mx-auto py-10">
         <div className="py-1">
-        <p className="text-center  text-sm text-black mt-[2px]">
-          &copy; Todos os direitos reservados, 2023
-        </p>
+          <p className="text-center  text-sm text-black mt-[2px]">
+            &copy; Todos os direitos reservados, 2023
+          </p>
         </div>
         <div className="text-center text-sm text-black py-1">
-        <span>Desenvolvido por:</span>
-        
-        
-        <Link target="_blank" className="text-center ml-1 p-1 border border-solid rounded shadow-md  hover:bg-blue-600 duration-300" href='https://www.linkedin.com/in/mateus-lins-061a5424a/'>Mateus Sales <LinkedinIcon  className="inline p-[1.5px] border border-black rounded-full mb-[2px] ml-[2px]" size={20} /></Link>
+          <span>Desenvolvido por:</span>
 
+          <Link
+            target="_blank"
+            className="text-center ml-1 p-1 border border-solid rounded shadow-md  hover:bg-blue-600 duration-300"
+            href={getLink()}
+          >
+            Mateus Sales{" "}
+            <Mail
+              className="inline p-0.5 border border-black rounded-full mb-[2px] ml-[2px]"
+              size={20}
+            />
+          </Link>
         </div>
       </div>
     </footer>
